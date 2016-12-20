1c0684f7f1a8908d3297733b9e15f937d4e74fd2
Erwin Majewski <erwin.majewski@gmail.com>
2016-12-20 17:03:15 +0100

    Add processes tree

---
+++ src/processes/Examples.java
@@ -0,0 +1,68 @@
+public class main {
+	
+	
+	public static void main(String[] args) {
+		
+		ProcessManager manager = new ProcessManager();
+		
+	//	manager.createFirstProcess("TEST");
+		
+		ProcessBlockController p1 = new ProcessBlockController(manager.getCounter(), "TEST");
+		manager.setRoot(p1);		
+		
+		ProcessBlockController p2 = new ProcessBlockController(manager.getCounter(),"Proces 1");
+		manager.createFirstProcess(p2);
+		
+		ProcessBlockController p3 = new ProcessBlockController(manager.getCounter(),"Proces 2");
+		manager.createFirstProcess(p3);
+		
+		ProcessBlockController p4 = new ProcessBlockController(manager.getCounter(),"Proces 3");
+		manager.createFirstProcess(p4);
+		
+		ProcessBlockController p5 = new ProcessBlockController(manager.getCounter(),"Proces 4");
+		manager.addProcess(p2,p5);
+		
+		ProcessBlockController p6 = new ProcessBlockController(manager.getCounter(),"Proces 5");
+		manager.addProcess(p2,p6);
+		
+		ProcessBlockController p7 = new ProcessBlockController(manager.getCounter(),"Proces 6");
+		manager.addProcess(p2,p7);
+		
+		ProcessBlockController p8 = new ProcessBlockController(manager.getCounter(),"Proces 7");
+		manager.createFirstProcess(p8);
+		
+		ProcessBlockController p9 = new ProcessBlockController(manager.getCounter(),"Proces 8");
+		manager.createFirstProcess(p9);
+		
+		ProcessBlockController p10 = new ProcessBlockController(manager.getCounter(),"Proces 9");
+		manager.createFirstProcess(p10);
+		
+		ProcessBlockController p11 = new ProcessBlockController(manager.getCounter(),"Proces 10");
+		manager.createFirstProcess(p11);
+		
+	//	ProcessBlockController p12 = new ProcessBlockController(manager.getCounter(),"Proces 11");
+		
+		// krotsze wywolanie metod
+		
+		manager.createFirstProcess("PROCES X");
+		manager.addProcess("PROCES Y", "PROCES X");
+		manager.addProcess("PROCES Z", "PROCES X");
+		manager.addProcess("PROCES A", "PROCES X");
+		System.out.println(manager.findNode("PROCES X").getName()+" HEREEEE");
+		System.out.println(manager.getChildrenNames("PROCES X"));
+		
+		
+		System.out.println(manager.getCounter());
+		System.out.println(manager.getRoot().getName());
+		System.out.println(manager.getRoot().getChildren());
+		System.out.println(manager.getRoot().getPID());
+		System.out.println(manager.isEmpty());
+		System.out.println(manager.getNumberOfProcesses(p1));
+		System.out.println(manager.getNumberOfProcesses(p2));
+		System.out.println(manager.getChildrenNames(p1));
+		if(manager.find(p2, p8)) System.out.println("DA"); 
+		p1.removeChild(p2);
+		System.out.println(manager.getChildrenNames(p1));
+	}
+
+}
---
+++ src/processes/ProcessBlockController.java
@@ -0,0 +1,101 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProcessBlockController {
+
+	private int PID;    // nr ID procesu
+	private String NAME; // nazwa procesu
+	private int PPID;	// nr ID rodzica procesu
+	private int STATE;	
+	
+	//  STATE=0 proces NOWY, po utworzeniu procesu.
+	//	STATE=1 proces WYKONYWANY, proces jest wykonywany.
+	//	STATE=2 proces OCZEKUJACY, proces czeka na przydzial zasobu innego niz procesor.
+	//	STATE=3 proces GOTOWY, proces czeka na przydzial procesora.
+	//  STATE=4 proces ZAKONCZONY, po zakonczeniu wykonywania procesu.
+	
+	int A;	// wartosci w rejestrach procesu
+	int B;
+	int C;
+	int D;
+	int E;
+	int F;
+	
+	private ProcessBlockController parent;			// rodzic procesu 
+	private List<ProcessBlockController> children;	// lista dzieci procesu
+
+
+	public ProcessBlockController(int counter, String name){
+		this.PID = counter;
+		this.NAME = name;
+		
+		this.children = new ArrayList<ProcessBlockController>();
+		
+		this.STATE = 0;
+		
+		if(this.getParent() != null)this.PPID = this.getParent().getPID();
+		else this.PPID = 0;
+	
+		this.A = 0;
+		this.B = 0;
+		this.C = 0;
+		this.D = 0;
+		this.E = 0;
+		this.F = 0;
+	}
+	
+	
+	public void addChild(ProcessBlockController child) {
+        child.setParent(this);
+        children.add(child);
+    }
+	
+	 public boolean removeChild(ProcessBlockController child) {
+	        List<ProcessBlockController> list = getChildren();
+	        return list.remove(child);
+	    }
+	
+	 public int getPID(){
+		 return this.PID;
+	 }
+
+	
+	public String getName() {
+	        return this.NAME;
+	  }
+
+	public void setName(String name) {
+	        this.NAME = name;
+	   }
+
+	public ProcessBlockController getParent() {
+	        return this.parent;
+	   }
+
+	public void setParent(ProcessBlockController parent) {
+	        this.parent = parent;
+	    }
+
+	public List<ProcessBlockController> getChildren() {
+	        return this.children;
+	    }
+	
+	public String getChildrenNames(){
+		String result="+ ";
+		List<ProcessBlockController> list= this.children;
+	for (ProcessBlockController pcb : list )
+		
+		result += pcb.getName() +" + ";
+		return result;
+	}
+	
+	public boolean equals(int PID){		
+		if (PID == this.PID) return true;
+		return false;
+	}
+	
+	public boolean equals(ProcessBlockController PCB){		
+		if (PCB == this) return true;
+		return false;
+	}
+}
