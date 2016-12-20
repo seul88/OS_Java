9337fa743360a28b7366de47b82bc85b19a0948b
Erwin Majewski <erwin.majewski@gmail.com>
2016-12-20 17:42:30 +0100

    Change of package

--- src/processes/ProcessBlockController.java
+++ src/processes/ProcessBlockController.java
@@ -1,3 +1,5 @@
+package src.put.os.tree;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -6,21 +8,21 @@ public class ProcessBlockController {
 	private int PID;    // nr ID procesu
 	private String NAME; // nazwa procesu
 	private int PPID;	// nr ID rodzica procesu
-	private int STATE;	
-	
+	private int STATE;
+
 	//  STATE=0 proces NOWY, po utworzeniu procesu.
 	//	STATE=1 proces WYKONYWANY, proces jest wykonywany.
 	//	STATE=2 proces OCZEKUJACY, proces czeka na przydzial zasobu innego niz procesor.
 	//	STATE=3 proces GOTOWY, proces czeka na przydzial procesora.
 	//  STATE=4 proces ZAKONCZONY, po zakonczeniu wykonywania procesu.
-	
+
 	int A;	// wartosci w rejestrach procesu
 	int B;
 	int C;
 	int D;
 	int E;
 	int F;
-	
+
 	private ProcessBlockController parent;			// rodzic procesu 
 	private List<ProcessBlockController> children;	// lista dzieci procesu
 
@@ -28,14 +30,14 @@ public class ProcessBlockController {
 	public ProcessBlockController(int counter, String name){
 		this.PID = counter;
 		this.NAME = name;
-		
+
 		this.children = new ArrayList<ProcessBlockController>();
-		
+
 		this.STATE = 0;
-		
+
 		if(this.getParent() != null)this.PPID = this.getParent().getPID();
 		else this.PPID = 0;
-	
+
 		this.A = 0;
 		this.B = 0;
 		this.C = 0;
@@ -43,58 +45,58 @@ public class ProcessBlockController {
 		this.E = 0;
 		this.F = 0;
 	}
-	
-	
-	public void addChild(ProcessBlockController child) {
-        child.setParent(this);
-        children.add(child);
-    }
-	
-	 public boolean removeChild(ProcessBlockController child) {
-	        List<ProcessBlockController> list = getChildren();
-	        return list.remove(child);
-	    }
-	
-	 public int getPID(){
-		 return this.PID;
-	 }
 
-	
+
+	public void addChild(ProcessBlockController child) {
+		child.setParent(this);
+		children.add(child);
+	}
+
+	public boolean removeChild(ProcessBlockController child) {
+		List<ProcessBlockController> list = getChildren();
+		return list.remove(child);
+	}
+
+	public int getPID(){
+		return this.PID;
+	}
+
+
 	public String getName() {
-	        return this.NAME;
-	  }
+		return this.NAME;
+	}
 
 	public void setName(String name) {
-	        this.NAME = name;
-	   }
+		this.NAME = name;
+	}
 
 	public ProcessBlockController getParent() {
-	        return this.parent;
-	   }
+		return this.parent;
+	}
 
 	public void setParent(ProcessBlockController parent) {
-	        this.parent = parent;
-	    }
+		this.parent = parent;
+	}
 
 	public List<ProcessBlockController> getChildren() {
-	        return this.children;
-	    }
-	
+		return this.children;
+	}
+
 	public String getChildrenNames(){
 		String result="+ ";
 		List<ProcessBlockController> list= this.children;
-	for (ProcessBlockController pcb : list )
-		
-		result += pcb.getName() +" + ";
+		for (ProcessBlockController pcb : list )
+
+			result += pcb.getName() +" + ";
 		return result;
 	}
-	
-	public boolean equals(int PID){		
+
+	public boolean equals(int PID){
 		if (PID == this.PID) return true;
 		return false;
 	}
-	
-	public boolean equals(ProcessBlockController PCB){		
+
+	public boolean equals(ProcessBlockController PCB){
 		if (PCB == this) return true;
 		return false;
 	}
