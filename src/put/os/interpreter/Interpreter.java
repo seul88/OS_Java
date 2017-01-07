44b2e913a1ed86a0e8790a2a0eabcb5837f6f978
Maciej Olejnik <maciej.olejnik@uptowork.com>
2017-01-07 15:56:33 +0100

    Program!
    Signed-off-by: Maciej Olejnik <maciej.olejnik@uptowork.com>

--- src/put/os/interpreter/Interpreter.java
+++ src/put/os/interpreter/Interpreter.java
@@ -15,6 +15,31 @@ public class Interpreter {
         public Command(int argLength) { this.argLength = argLength; }
         public int getArgLength() { return argLength; }
 
+        public int getSource(String arg) {
+            try
+            {
+                return Integer.parseInt(arg);
+            }
+            catch(NumberFormatException nfe)
+            {
+                switch (arg) {
+                    case "A":
+                        return Processor.A;
+                    case "B":
+                        return Processor.B;
+                    case "C":
+                        return Processor.C;
+                    case "D":
+                        return Processor.D;
+                    case "E":
+                        return Processor.E;
+                    case "F":
+                        return Processor.F;
+                    default:
+                        return 0;
+                }
+            }
+        }
         public boolean execute(Vector<String> arg) { return false; }
     }
     private static Map<String, Command> commands = new HashMap<>();
@@ -29,31 +54,6 @@ public class Interpreter {
 
         // MOV
         commands.put("MOV", new Command(2) {
-            private int getSource(String arg) {
-                try
-                {
-                    return Integer.parseInt(arg);
-                }
-                catch(NumberFormatException nfe)
-                {
-                    switch (arg) {
-                        case "A":
-                            return Processor.A;
-                        case "B":
-                            return Processor.B;
-                        case "C":
-                            return Processor.C;
-                        case "D":
-                            return Processor.D;
-                        case "E":
-                            return Processor.E;
-                        case "F":
-                            return Processor.F;
-                        default:
-                            return 0;
-                    }
-                }
-            }
 
             @Override
             public boolean execute(Vector<String> arg) {
@@ -87,6 +87,197 @@ public class Interpreter {
                     }
                 }
             }
+
+        });
+
+        // MP
+        commands.put("MP", new Command(2) {
+
+            @Override
+            public boolean execute(Vector<String> arg) {
+                switch (arg.get(0)) {
+                    case "A": {
+                        Processor.A *= getSource(arg.get(1));
+                        return true;
+                    }
+                    case "B": {
+                        Processor.B *= getSource(arg.get(1));
+                        return true;
+                    }
+                    case "C": {
+                        Processor.C *= getSource(arg.get(1));
+                        return true;
+                    }
+                    case "D": {
+                        Processor.D *= getSource(arg.get(1));
+                        return true;
+                    }
+                    case "E": {
+                        Processor.E *= getSource(arg.get(1));
+                        return true;
+                    }
+                    case "F": {
+                        Processor.F *= getSource(arg.get(1));
+                        return true;
+                    }
+                    default: {
+                        return false;
+                    }
+                }
+            }
+
+        });
+
+        // ADD
+        commands.put("ADD", new Command(2) {
+
+            @Override
+            public boolean execute(Vector<String> arg) {
+                switch (arg.get(0)) {
+                    case "A": {
+                        Processor.A += getSource(arg.get(1));
+                        return true;
+                    }
+                    case "B": {
+                        Processor.B += getSource(arg.get(1));
+                        return true;
+                    }
+                    case "C": {
+                        Processor.C += getSource(arg.get(1));
+                        return true;
+                    }
+                    case "D": {
+                        Processor.D += getSource(arg.get(1));
+                        return true;
+                    }
+                    case "E": {
+                        Processor.E += getSource(arg.get(1));
+                        return true;
+                    }
+                    case "F": {
+                        Processor.F += getSource(arg.get(1));
+                        return true;
+                    }
+                    default: {
+                        return false;
+                    }
+                }
+            }
+
+        });
+
+        // SB
+        commands.put("SB", new Command(2) {
+
+            @Override
+            public boolean execute(Vector<String> arg) {
+                switch (arg.get(0)) {
+                    case "A": {
+                        Processor.A -= getSource(arg.get(1));
+                        return true;
+                    }
+                    case "B": {
+                        Processor.B -= getSource(arg.get(1));
+                        return true;
+                    }
+                    case "C": {
+                        Processor.C -= getSource(arg.get(1));
+                        return true;
+                    }
+                    case "D": {
+                        Processor.D -= getSource(arg.get(1));
+                        return true;
+                    }
+                    case "E": {
+                        Processor.E -= getSource(arg.get(1));
+                        return true;
+                    }
+                    case "F": {
+                        Processor.F -= getSource(arg.get(1));
+                        return true;
+                    }
+                    default: {
+                        return false;
+                    }
+                }
+            }
+
+        });
+
+        // INC
+        commands.put("INC", new Command(1) {
+
+            @Override
+            public boolean execute(Vector<String> arg) {
+                switch (arg.get(0)) {
+                    case "A": {
+                        Processor.A++;
+                        return true;
+                    }
+                    case "B": {
+                        Processor.B++;
+                        return true;
+                    }
+                    case "C": {
+                        Processor.C++;
+                        return true;
+                    }
+                    case "D": {
+                        Processor.D++;
+                        return true;
+                    }
+                    case "E": {
+                        Processor.E++;
+                        return true;
+                    }
+                    case "F": {
+                        Processor.F++;
+                        return true;
+                    }
+                    default: {
+                        return false;
+                    }
+                }
+            }
+
+        });
+
+        // DEC
+        commands.put("DEC", new Command(1) {
+
+            @Override
+            public boolean execute(Vector<String> arg) {
+                switch (arg.get(0)) {
+                    case "A": {
+                        Processor.A--;
+                        return true;
+                    }
+                    case "B": {
+                        Processor.B--;
+                        return true;
+                    }
+                    case "C": {
+                        Processor.C--;
+                        return true;
+                    }
+                    case "D": {
+                        Processor.D--;
+                        return true;
+                    }
+                    case "E": {
+                        Processor.E--;
+                        return true;
+                    }
+                    case "F": {
+                        Processor.F--;
+                        return true;
+                    }
+                    default: {
+                        return false;
+                    }
+                }
+            }
+
         });
     }
 
@@ -116,6 +307,8 @@ public class Interpreter {
 
         String command = tokens.get(0).toString();
 
+        System.out.println(command);
+
         // Check command
         if(!commands.containsKey(command))
         {
@@ -144,10 +337,7 @@ public class Interpreter {
             if(data != ' ' && data != '\n')
             {
                 tokens.get(argIndex+1).append(data);
-            }
-            else
-            {
-               ++argIndex;
+                ++argIndex;
             }
         }
 
